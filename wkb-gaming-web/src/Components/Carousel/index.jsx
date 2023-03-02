24882d2710e8dadf0488218717e819337ac984ee
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2023-03-02 14:20:05 -0300

    Add/Components

---
+++ wkb-gaming-web/src/Components/Carousel/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Styles from './Carousel.module.css'
+
+export const Carousel = () => {
+    <div>Carousel</div>
+}
---
+++ wkb-gaming-web/src/Components/Why/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Styles from './Why.module.css'
+export const Why = () => {
+    return (
+        <div>
+          Why us? 
+        </div>
+    )
+}
