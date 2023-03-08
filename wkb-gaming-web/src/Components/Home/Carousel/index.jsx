577c97d34ae7b4f60b9f4412336dfeecded3f3c5
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2023-03-08 02:08:07 -0300

    Add/CarrouselComponent

--- wkb-gaming-web/src/Components/Home/Carousel/index.jsx
+++ wkb-gaming-web/src/Components/Home/Carousel/index.jsx
@@ -1,6 +1,93 @@
-import React from 'react';
-import Styles from './Carousel.module.css'
+import React from "react";
+import Arvum from "../../../assets/HOME/OUR PROJECTS GALLERY/Project_ ARVUM ESPORTS-min.png";
+import Caribe from "../../../assets/HOME/OUR PROJECTS GALLERY/Project_ CARIBE SCHOOL-min.png";
+import Coworking from "../../../assets/HOME/OUR PROJECTS GALLERY/Project_ COWORKING SLA-min.png";
+import Lineage from "../../../assets/HOME/OUR PROJECTS GALLERY/Project_ LINEAGE 2-min.png";
+import OnrBranding from "../../../assets/HOME/OUR PROJECTS GALLERY/Project_ ONR BRANDING-min.png";
+import OnrMarketing from "../../../assets/HOME/OUR PROJECTS GALLERY/Project_ ONR MARKETING-min.png";
+import OnrWebsite from "../../../assets/HOME/OUR PROJECTS GALLERY/Project_ ONR WEBSITE-min.png";
+import SaltaGaming from "../../../assets/HOME/OUR PROJECTS GALLERY/Project_ SALTA GAMING-min.png";
+import Slider from "react-slick";
+import Styles from "./Carousel.module.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const Carousel = () => {
-    <div>Carousel</div>
-}
+  const settings = {
+    dots: false,
+    infinite: true,
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1500,
+    autoplaySpeed: 2000,
+    margin: "200px",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const settings2 = {
+    dots: false,
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1600,
+    autoplaySpeed: 2000,
+    margin: "200px",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div>
+      <div className={Styles.sliderContainer}>
+        <Slider {...settings}>
+          <div className={Styles.slide}>
+            <img src={Coworking} alt="" />
+          </div>
+          <div className={Styles.slide}>
+            <img src={Lineage} alt="" />
+          </div>
+          <div className={Styles.slide}>
+            <img src={OnrBranding} alt="" />
+          </div>
+          <div className={Styles.slide}>
+            <img src={SaltaGaming} alt="" />
+          </div>
+        </Slider>
+      </div>
+      <div style={{ width: "100%" }}>
+        <Slider {...settings2}>
+          <div className={Styles.slide}>
+            <img src={Arvum} alt="" />
+          </div>
+          <div className={Styles.slide}>
+            <img src={Caribe} alt="" />
+          </div>
+          <div className={Styles.slide}>
+            <img src={OnrMarketing} alt="" />
+          </div>
+          <div className={Styles.slide}>
+            <img src={OnrWebsite} alt="" />
+          </div>
+        </Slider>
+      </div>
+    </div>
+  );
+};
--- wkb-gaming-web/src/Components/Home/Why/index.jsx
+++ wkb-gaming-web/src/Components/Home/Why/index.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
-import Styles from './Why.module.css'
+import React from "react";
+import Styles from "./Why.module.css";
+import {BsChevronDown} from 'react-icons/bs'
 export const Why = () => {
-    return (
-        <div>
-          Why us? 
+  return (
+    <div className={Styles.container}>
+      <div>
+        <p>Why us?</p>
+      </div>
+      <div>
+        <div className={Styles.info}>
+          <div className={Styles.subtitle}>
+            <h5>Top Talents</h5>
+            <BsChevronDown/>
+          </div>
+          <p>
+            We work with the best and brightest tech talent to ensure you get a
+            quality service.
+          </p>
         </div>
-    )
-}
+        <div className={Styles.info}>
+          <div className={Styles.subtitle}>
+            <h5>Proven experience</h5>
+            <BsChevronDown/>
+          </div>
+
+          <p>
+            We have helped projects and companies to solve their digital needs,
+            from streamlining or implementing processes through a
+            system/software, to generating a strategy of complete marketing, for
+            positioning or maintenance of the brand.
+          </p>
+        </div>
+        <div className={Styles.info}>
+          <div className={Styles.subtitle}>
+            <h5>Full Focus</h5>
+            <BsChevronDown/>
+          </div>
+
+          <p>
+            We work focused on your vision, your objectives, and we help create
+            a plan, ordered to execute it together.
+          </p>
+        </div>
+        <div className={Styles.info}>
+          <div className={Styles.subtitle}>
+            <h5>Real commitment</h5>
+            <BsChevronDown/>
+          </div>
+
+          <p>
+            Our main objective is that you nd a team willing to help your
+            project grow, or join your team to enhance it.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
