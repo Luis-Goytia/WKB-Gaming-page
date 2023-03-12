db5ef9ae0090faf08cb91622f80529cdfc35f04f
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2023-03-12 17:17:30 -0300

    Add/CoponentsServices

---
+++ wkb-gaming-web/src/Components/Services/EsportServices/EsportDigital/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Game1 from '../../../../assets/Services/img/AppGames/'
+import Game2 from '../../../../assets/Services/img/AppGames/'
+import Game3 from '../../../../assets/Services/img/AppGames/'
+import Game4 from '../../../../assets/Services/img/AppGames/'
+
+export const MobileApp = () => {
+  return (
+    <div>
+      <div>
+        <h1>Mobile App Development</h1>
+      </div>
+      <div>
+        <p>
+          We help you to became an idea to real app using programming for your
+          project.
+        </p>
+        <p>
+          We set up the development process from scratch and take full
+          responsibility for the project risks: from timelines and budget to the
+          final software quality.
+        </p>
+      </div>
+      <div>
+        <div>imagen1</div>
+        <div>imagen2</div>
+        <div>imagen3</div>
+        <div>imagen4</div>
+      </div>
+      <div>
+        <div>
+          <h2>Problem</h2>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h2>How is our workflow</h2>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h2>Key Benefits</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
