083997fe0349971e227f042cd6ee475e6588c126
gonzalobordon <36607267+gonzalobordon@users.noreply.github.com>
2023-03-11 23:13:40 -0300

    anim-rotate

--- wkb-gaming-web/src/Components/Experience/World.jsx
+++ wkb-gaming-web/src/Components/Experience/World.jsx
@@ -19,39 +19,32 @@ import { Bear } from './Bear';
 import { Rocks } from './Rocks';
 import Camera from './Camera';
 import Pyramid from './Pyramid';
-import { Suspense, useState } from 'react';
+import { Suspense, useRef, useState } from 'react';
 import './World.module.css';
 import { WKBPlanet } from './WKBPlanet';
-
-function Rig() {
-  const [vec] = useState(() => new THREE.Vector3());
-  const { camera, mouse } = useThree();
-  useFrame(() =>
-    camera.position.lerp(
-      vec.set(mouse.x * 0.25, mouse.y * 0.25, camera.position.z),
-      0.0125
-    )
-  );
-  return (
-    <CameraShake
-      maxYaw={0.01}
-      maxPitch={0.01}
-      maxRoll={0.01}
-      yawFrequency={0.5}
-      pitchFrequency={0.5}
-      rollFrequency={0.4}
-    />
-  );
-}
+import Overlay from './OverlayWorld';
 
 export default function World() {
+  const overlay = useRef();
+  const caption = useRef();
+  const scroll = useRef(0);
   return (
     <>
       <h1>WKB GAMING</h1>
-      <Canvas dpr={[0.5, 15]} shadows>
-        <fog attach="fog" args={['#272730', -2, 10]} />
+      <Canvas
+        dpr={[0.5, 15]}
+        shadows
+        eventSource={document.getElementById('root')}
+        eventPrefix="client"
+      >
+        <fog attach="fog" args={['#272730', -1.75, 7]} />
         <ambientLight intensity={0.75} />
-        <PerspectiveCamera makeDefault position={[0, 0, 3.75]} fov={60}>
+
+        <Environment preset="night" />
+        <Suspense fallback={null}>
+          <WKBPlanet />
+        </Suspense>
+        <PerspectiveCamera makeDefault position={[0, 0, 3.5]} fov={60}>
           <pointLight intensity={2} position={[-10, -25, -10]} />
           <spotLight
             castShadow
@@ -63,17 +56,6 @@ export default function World() {
             shadow-bias={-0.0001}
           />
         </PerspectiveCamera>
-        <Environment preset="night" />
-        <Suspense fallback={null}>
-          <WKBPlanet />
-        </Suspense>
-        <OrbitControls
-          autoRotate
-          enablePan={false}
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
         <Stars radius={500} depth={50} count={350} factor={10} />
       </Canvas>
       <div className="layer" />
