bd04a385f5a43bcdf4baf2b6c91966ea6dfc5885
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2023-03-15 16:23:51 -0300

    testing deploy

--- wkb-gaming-web/vite.config.js
+++ wkb-gaming-web/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+//export default defineConfig({
+ //plugins: [react()],
+//})
+
+export default {
+  plugins: [reactRefresh()],
+  server: {
+    hmr: {
+      port: 443,
+    },
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+}
