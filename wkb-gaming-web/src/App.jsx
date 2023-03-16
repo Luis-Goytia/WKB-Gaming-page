9ece746a7cd3b9260971e06e46d4dd3ddbbfc65f
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2023-03-16 01:09:17 -0300

    noseinunda-mas

--- wkb-gaming-web/src/App.jsx
+++ wkb-gaming-web/src/App.jsx
@@ -1,20 +1,22 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import { Home } from './Components/Home';
-import { AppGames } from './Components/Services/AppGames';
-import { EsportServices } from './Components/Services/EsportServices';
-import { Marketing } from './Components/Services/Marketing';
-import { ProductDesing } from './Components/Services/ProductDesing';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from "./Components/Home";
+import { AppGames } from "./Components/Services/AppGames";
+import { EsportServices } from "./Components/Services/EsportServices";
+import { Marketing } from "./Components/Services/Marketing";
+import { ProductDesing } from "./Components/Services/ProductDesing";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Services/AppGames" element={<AppGames />} />
-        <Route path="/Services/EsportServices" element={<EsportServices />} />
-        <Route path="/Services/Marketing" element={<Marketing />} />
-        <Route path="/Services/ProductDesing" element={<ProductDesing />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Services/AppGames" element={<AppGames />} />
+          <Route path="/Services/EsportServices" element={<EsportServices />} />
+          <Route path="/Services/Marketing" element={<Marketing />} />
+          <Route path="/Services/ProductDesing" element={<ProductDesing />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
